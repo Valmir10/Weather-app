c47f16054db4ccd464e53d05e60f60793ecd2f46
Valmir10 <valmir8@hotmail.se>
2025-08-24 13:00:14 +0200

    Add Header component files for WeatherDisplay

--- frontend/src/components/WeatherDisplay.jsx
+++ frontend/src/components/WeatherDisplay.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "../styles/WeatherDisplay.css";
+import Header from "./Header";
 
 const WeatherDisplay = () => {
   return (
     <main className="weather-display-container">
-      <h1>Weather Display</h1>
+      <Header />
     </main>
   );
 };
