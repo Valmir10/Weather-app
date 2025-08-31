327151a9d07f1c2aa5a76275bbe1ebb7c2d072bf
Valmir10 <valmir8@hotmail.se>
2025-08-31 20:16:33 +0200

    Header Structure completed

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import "../styles/Header.css";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <header className="header-section-container">
+      <div className="search-container">
+        <label htmlFor="search-input" className="search-label-container">
+          <input type="text" id="search-input" placeholder="Search..." />
+        </label>
+      </div>
+    </header>
   );
 };
 
